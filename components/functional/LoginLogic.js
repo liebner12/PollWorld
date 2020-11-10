77e5142eec2638fee316f4ac7fab55a8a7739e20
Seb-Maraszek <sebastian.maraszek0@gmail.com>
2020-11-10 19:22:06 +0100

    Tokens are ready to be sent to backend

--- components/functional/LoginLogic.js
+++ components/functional/LoginLogic.js
@@ -3,22 +3,29 @@ import * as Facebook from "expo-facebook";
 import * as Google from 'expo-google-app-auth';
 import Constants from "expo-constants";
 
-const signInWithGoogle = async () => {
-    const result = await Google.logInAsync({
-        androidClientId: Constants.manifest.extra.androidClientId,
-        scopes: ["profile", "email"]
-    });
-    if (result.type === "success") {
-        this.setState({
-            name: result.user.name,
-            signedIn: true
-        })
-    }
-}
+const signIn = {
+    async signInWithGoogle(){
+        const scopes = ["profile", "email"];
+        const {type, accessToken, user} = await Google.logInAsync({
+            androidClientId: "749206604554-mk3k4vfvo4jl390mmspjnj6570il9vq6.apps.googleusercontent.com",
+            scopes: scopes
+        });
+        if (type === "success") {
+            console.log(accessToken)
+            //TODO wysłać accessToken do backendu
+        }
+    },
 
-const signInWithFacebook = async () => {
-    const appId = Constants.manifest.extra.appId;
-    const permissions = ["public_profile", "email"];
-    await Facebook.initializeAsync({appId});
-    const { type, token } = await Facebook.logInWithReadPermissionsAsync({permissions});
+    async signInWithFacebook(){
+        const appId = "276251297112045";
+        const permissions = ["public_profile", "email"];
+        await Facebook.initializeAsync({appId});
+        const { type, token } = await Facebook.logInWithReadPermissionsAsync({permissions});
+
+        if (type === "success") {
+            console.log(token)
+            //TODO wysłać accessToken do backendu
+        }}
+
 }
+export default signIn
