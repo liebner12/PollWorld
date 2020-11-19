3e101b4eeb7011c2e3d36bd65f79b05782e4518e
liebner12 <liebner15@gmail.com>
2020-11-19 13:58:22 +0100

    Refactor

--- components/common/Alert.js
+++
@@ -1,7 +0,0 @@
-import { ToastAndroid } from "react-native";
-
-export const Alert = () => {
-  return ToastAndroid.show("A pikachu appeared nearby !", ToastAndroid.SHORT);
-};
-
-export default Alert;
