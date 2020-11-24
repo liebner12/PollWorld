985882d45b2b7ff8774cc0a08bf9c776289bdb54
Seb-Maraszek <sebastian.maraszek0@gmail.com>
2020-11-24 20:03:02 +0100

    Added function that retrieves surveys from server (so far from mock).

--- components/functional/api/fetchBuilder.js
+++ components/functional/api/fetchBuilder.js
@@ -1,4 +1,4 @@
-const server = `http://5104c283a4a7.ngrok.io`;
+const server = `http://d2fdf2d8b31e.ngrok.io`;
 
 const getHeaders = (token) => {
   const headers = {
@@ -38,3 +38,31 @@ export const post = async (destination, body, token) => {
     response_headers: response_headers
   };
 }
+export const get = async (destination, body, token) => {
+  let response_status;
+  let response_headers;
+  let response_body;
+  let res;
+  await fetch(`${server}${destination}`,{
+    method: "GET",
+    headers: getHeaders(token),
+    body: await JSON.stringify(body),}
+  ) .catch((error) => {
+    console.error(error);
+  })
+      .then(response => {
+        response_status = response.status;
+        response_headers = response.headers;
+        res = response})
+      .catch((error) => {
+        console.error(error);
+      });
+  response_body = await res.json()
+  return {
+    response_body: response_body,
+    response_status: response_status,
+    response_headers: response_headers
+  };
+}
+
+
--- components/functional/authentication/logic/socialMediaSignIn.js
+++ components/functional/authentication/logic/socialMediaSignIn.js
@@ -8,26 +8,25 @@ import {initializeAsync, logInWithReadPermissionsAsync} from "expo-facebook";
 
 export const getGoogleUserToken = async() =>{
         const scopes = ["profile", "email"];
-        const {type, token, user} = await Google.logInAsync({
+        const result = await Google.logInAsync({
             androidClientId: googleAppId,
             scopes: scopes
         }) .catch((error) => {
             console.error(error);
         });
+        console.log(result.accessToken);
+        const user =  result.user;
         return user;
     };
 
 export const getFacebookUserToken = async() =>{
         await initializeAsync({appId: facebookAppId}).catch((error) =>{ console.log((error))});
-        const {
-            type,
-            token,
-    } = await logInWithReadPermissionsAsync({
+       const result = await logInWithReadPermissionsAsync({
         permissions: ["public_profile", "email"],
     }).then() .catch((error) => {
             console.error(error);
         });
-        console.log(token)
-        return token;
+        console.log(result.token)
+        return result.token;
 }
 
